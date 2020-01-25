2ec1cd8cf3b259454664876fbdaefd1f412a6218
Meghna Krishnan <meghna.krishnan@uoit.net>
2020-01-24 22:28:17 -0500

    Finished file

--- mycp.cpp
+++ mycp.cpp
@@ -1,29 +1,43 @@
+// Name: Meghna Krishnan
+// Student Id: 100696561
+// Date: 2020-01-21
+// Program: mycp.cpp
+// Purpose: Implementing I/O to files
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std; 
+
 int main(int argc, char const *argv[])
 {
+	// check if there are enough arguments
+	if (argc < 3) {
+		cerr << "There are not enough arguments." << endl;
+		return 1;
+	}
+
+	char c;
+
  	ifstream fin;
  	ofstream fout;
 	
-	// check if there are enough arguments
-	
-	
 	// open the first file
- 	
-	char c;
+	fin.open(argv[1]);
 
- 	if (fin.fail()) // check if it is successful 
- 	{
- 		cerr << " Cannot open the input file!" << endl;
- 		return 1;
- 	}
- 	
+    if (fin.fail()) {  // check if it is successful
+        cerr << "Cannot open the input file!" << endl;
+        return 1;
+    }
 
 	// open the second file
-	
- 	if (fout.fail())
- 	{
- 		cerr << " Cannot open the output file!" << endl;
- 		return 1;
- 	}
+    fout.open(argv[2]);
+
+    if (fout.fail()) {  // check if it is successful
+        cerr << "Cannot open the output file!" << endl;
+        return 1;
+    }
  	
  	while(fin.get(c)) 
 	{
@@ -31,9 +45,7 @@ int main(int argc, char const *argv[])
 	}
  	
  	fin.close(); 
-
  	fout.close();
 
  	 return 0;
-
  } 
